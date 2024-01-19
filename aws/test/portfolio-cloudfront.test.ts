358871d829e7a53272aa3132716ef5e3bdd2f8fe
Melle <melle@git.com>
2024-01-19 16:29:56 +0100

    Lint file

--- aws/test/portfolio-cloudfront.test.ts
+++ aws/test/portfolio-cloudfront.test.ts
@@ -3,7 +3,10 @@ import { PortfolioCloudfrontStack } from "../lib/portfolio-cloudfront-stack";
 
 test("Match with snapshot", () => {
   const app = new App();
-  const stack = new PortfolioCloudfrontStack(app, "TestPortfolioCloudfrontStack");
+  const stack = new PortfolioCloudfrontStack(
+    app,
+    "TestPortfolioCloudfrontStack",
+  );
   const template = assertions.Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
 });
