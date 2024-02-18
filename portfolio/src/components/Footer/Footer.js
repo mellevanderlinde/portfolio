7703248d5c21a77313e090e6b08b6f041694de0b
Melle <melle@git.com>
2024-02-18 22:13:59 +0100

    First version of redesigned website

---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Created by Raj Shekhar
+    </a>
+  </footer>
+)
+
+export default Footer
