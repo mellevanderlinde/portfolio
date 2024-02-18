96c2236fdf065bdc5d3259be59b66e4bd4410aa1
Melle <melle@git.com>
2024-02-18 23:59:04 +0100

    Remove link

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -2,12 +2,7 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/rajshekhar26/cleanfolio'
-      className='link footer__link'
-    >
-      Created by Raj Shekhar
-    </a>
+    Created by Raj Shekhar
   </footer>
 )
 
