ddf41d07cc3acb06962f740c6b738466398e401d
Melle <melle@git.com>
2024-07-13 12:20:23 +0200

    Add linter for portfolio folder

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,16 +1,16 @@
-import './styles/index.css';
-import scroll from './modules/scroll';
-import { toggleTheme, addThemeClass } from './modules/toggleTheme';
-import displayList from './modules/displayList';
+import "./styles/index.css";
+import scroll from "./modules/scroll";
+import { toggleTheme, addThemeClass } from "./modules/toggleTheme";
+import displayList from "./modules/displayList";
 
-const btnHamburger = document.querySelector('.fa-bars');
-const btnTheme = document.querySelector('.fa-moon');
+const btnHamburger = document.querySelector(".fa-bars");
+const btnTheme = document.querySelector(".fa-moon");
 
-btnHamburger.addEventListener('click', displayList);
-btnTheme.addEventListener('click', toggleTheme);
-document.addEventListener('scroll', scroll.scrollUp);
+btnHamburger.addEventListener("click", displayList);
+btnTheme.addEventListener("click", toggleTheme);
+document.addEventListener("scroll", scroll.scrollUp);
 
-const getBodyClass = localStorage.getItem('class-body-theme');
-const getBtnClass = localStorage.getItem('class-btn-theme');
+const getBodyClass = localStorage.getItem("class-body-theme");
+const getBtnClass = localStorage.getItem("class-btn-theme");
 
 addThemeClass(getBodyClass, getBtnClass);
