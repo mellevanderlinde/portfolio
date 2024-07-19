329f4b10b4e6e199a7b6b2e98930191cb63a972c
Melle <melle@git.com>
2024-07-19 13:58:49 +0200

    Consistent URLs

--- portfolio/app/components/footer.tsx
+++ portfolio/app/components/footer.tsx
@@ -38,7 +38,7 @@ export function Footer() {
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         {createFooterItem("github", "https://github.com/mellevanderlinde")}
-        {createFooterItem("linkedin", "https://linkedin.com/in/mellevdlinde/")}
+        {createFooterItem("linkedin", "https://linkedin.com/in/mellevdlinde")}
         {createFooterItem(
           "view source",
           "https://github.com/mellevanderlinde/portfolio-cloudfront",
