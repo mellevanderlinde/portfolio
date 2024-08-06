c981ed2dc51df15a50186bb2a9303a8e44edc68c
Melle <melle@git.com>
2024-08-06 19:53:06 +0200

    Add projects

--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Header1, Header2 } from "app/components/header";
 import { ReactElement } from "react";
-import { Paragraph } from "app/components/paragraph";
 import { HorizontalLine } from "app/components/line";
 import { Badge } from "app/components/bagde";
 
@@ -11,18 +10,19 @@ export const metadata: Metadata = {
 
 function ProjectItem(
   title: string,
-  description: string,
   url: string,
+  content: ReactElement,
 ): ReactElement {
   return (
     <div>
       <Header2 text={title} />
-      <Paragraph text={description} />
+      {content}
+      <p className="mb-1" />
       <Badge href={url}>
         <svg width="13" height="11" role="img" className="mr-1 inline-flex">
           <use href="/sprite.svg#github" />
         </svg>
-        Repository
+        GitHub
       </Badge>
     </div>
   );
@@ -34,20 +34,26 @@ export default function ProjectsPage(): ReactElement {
       <Header1 text="my projects" />
       {ProjectItem(
         "Bedrock API",
-        "AWS CDK project that deploys an API endpoint to interact with a large language model from Amazon's generative AI service.",
         "https://github.com/mellevanderlinde/bedrock-apigateway",
+        <section>
+          <p className="prose prose-neutral dark:prose-invert">{`AWS CDK project that deploys an API endpoint to interact with a large language model from Amazon's generative AI service.`}</p>
+        </section>,
       )}
       <HorizontalLine />
       {ProjectItem(
         "CloudFront Portfolio",
-        "AWS CDK project that serves this portfolio with CloudFront. The portfolio design is a template from Vercel.",
         "https://github.com/mellevanderlinde/portfolio-cloudfront",
+        <section>
+          <p className="prose prose-neutral dark:prose-invert">{`AWS CDK project that serves this portfolio with CloudFront. The portfolio design is a template from Vercel.`}</p>
+        </section>,
       )}
       <HorizontalLine />
       {ProjectItem(
         "Airflow on AWS",
-        "AWS CDK project that deploys an MWAA environment for Airflow and an example DAG.",
         "https://github.com/mellevanderlinde/mwaa-airflow",
+        <section>
+          <p className="prose prose-neutral dark:prose-invert">{`AWS CDK project that deploys an MWAA environment for Airflow and an example DAG.`}</p>
+        </section>,
       )}
     </section>
   );
