5d38b56c75aba3cac696b0bc72d853b92549e97e
Melle <melle@git.com>
2024-09-02 18:08:03 +0200

    Apply destroy removalPolicy to entire stack

--- aws/bin/portfolio-cloudfront.ts
+++ aws/bin/portfolio-cloudfront.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import { App } from "aws-cdk-lib";
+import { App, Aspects, IAspect, CfnResource, RemovalPolicy } from "aws-cdk-lib";
+import { IConstruct } from "constructs";
 import { PortfolioCloudfrontStack } from "../lib/portfolio-cloudfront-stack";
+
+class RemovalPolicyDestroyAspect implements IAspect {
+  visit(node: IConstruct): void {
+    if (CfnResource.isCfnResource(node)) {
+      node.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    }
+  }
+}
 
 const app = new App();
 new PortfolioCloudfrontStack(app, "PortfolioCloudfrontStack", {
@@ -10,3 +19,4 @@ new PortfolioCloudfrontStack(app, "PortfolioCloudfrontStack", {
     region: "us-east-1", // required, see https://docs.aws.amazon.com/acm/latest/userguide/acm-regions.html
   },
 });
+Aspects.of(app).add(new RemovalPolicyDestroyAspect());
