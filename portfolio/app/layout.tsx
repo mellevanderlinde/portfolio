f46c0d19ad3ee325cca47170360d3ed2be5ad85f
Melle <melle@git.com>
2024-09-03 14:51:56 +0200

    Cleanup layout.tsx

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
 import { ReactElement } from "react";
 import Footer from "./components/footer";
@@ -15,8 +13,6 @@ export const metadata: Metadata = {
   },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(" ");
-
 export default function RootLayout({
   children,
 }: {
@@ -25,11 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx(
-        "text-black bg-white dark:text-white dark:bg-[#111010]",
-        GeistSans.variable,
-        GeistMono.variable,
-      )}
+      className="text-black bg-white dark:text-white dark:bg-[#111010]"
     >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
