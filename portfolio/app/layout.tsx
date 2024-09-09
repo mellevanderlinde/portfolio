c02537745d545ece8d75e574ad693809e6d496df
Melle <melle@git.com>
2024-09-09 17:20:45 +0200

    Improve typing

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./global.css";
 import type { Metadata } from "next";
 import { Navbar } from "./components/nav";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
 
@@ -13,10 +13,8 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+export default function RootLayout(props: {
+  children: ReactNode;
 }): ReactElement {
   return (
     <html
@@ -26,7 +24,7 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
-          {children}
+          {props.children}
           <Footer />
         </main>
       </body>
