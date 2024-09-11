58e9d756dec04be45992cc90a82567dddb7b603b
Melle <melle@git.com>
2024-09-11 20:06:14 +0200

    Add metadata to 404 page

--- portfolio/app/not-found.tsx
+++ portfolio/app/not-found.tsx
@@ -1,11 +1,18 @@
 import { Paragraph } from "./components/paragraph";
 import { Header1 } from "./components/header";
 import { ReactElement } from "react";
+import { Metadata } from "next";
+
+const title = "404 - Page not found";
+
+export const metadata: Metadata = {
+  title,
+};
 
 export default function NotFound(): ReactElement {
   return (
     <section>
-      <Header1 title="404 - page not found" />
+      <Header1 title={title} />
       <Paragraph content="The page you are looking for does not exist." />
     </section>
   );
