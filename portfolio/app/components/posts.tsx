1a11666fca713a2f57f498835bc9a5db953cd9af
Melle <melle@git.com>
2024-09-11 21:32:24 +0200

    Add view transitions for blog's title and date

--- portfolio/app/components/posts.tsx
+++ portfolio/app/components/posts.tsx
@@ -23,10 +23,16 @@ export function BlogPosts(): ReactElement {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              <p
+                className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums"
+                style={{ viewTransitionName: `post-date-${post.slug}` }}
+              >
                 {formatDate(post.metadata.publishedAt)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p
+                className="text-neutral-900 dark:text-neutral-100 tracking-tight"
+                style={{ viewTransitionName: `post-title-${post.slug}` }}
+              >
                 {post.metadata.title}
               </p>
             </div>
