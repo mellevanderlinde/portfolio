dd0353f541f7aeca5419c434c2ca6df821a78f52
Melle <melle@git.com>
2024-09-12 17:00:39 +0200

    Remove view transitions

--- portfolio/app/components/posts.tsx
+++ portfolio/app/components/posts.tsx
@@ -1,4 +1,4 @@
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 import { formatDate, getBlogPosts } from "app/blog/utils";
 import { ReactElement } from "react";
 
@@ -23,16 +23,10 @@ export function BlogPosts(): ReactElement {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p
-                className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums"
-                style={{ viewTransitionName: `post-date-${post.slug}` }}
-              >
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 {formatDate(post.metadata.publishedAt)}
               </p>
-              <p
-                className="text-neutral-900 dark:text-neutral-100 tracking-tight"
-                style={{ viewTransitionName: `post-title-${post.slug}` }}
-              >
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
             </div>
