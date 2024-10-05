5dac98437fffed4613d715d089424a1a0f0ea051
Melle <melle@git.com>
2024-10-05 23:13:32 +0200

    Remove quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,7 @@ export default [
       "semi": ["error", "always"],
       "quotes": ["error", "double"],
       "no-unused-vars": "off",
-      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
       "no-trailing-spaces": ["error"],
       "comma-dangle": ["error", "always-multiline"],
       "@typescript-eslint/no-unused-vars": ["error"],
