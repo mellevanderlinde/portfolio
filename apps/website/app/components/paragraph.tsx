368c0ff19476226c3287f7895734d09b21005003
Melle <melle@git.com>
2024-10-12 21:55:13 +0200

    Remove

--- apps/website/app/components/paragraph.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export function Paragraph({ content }): ReactNode {
-  return (
-    <p className="mb-2 prose prose-neutral dark:prose-invert">{content}</p>
-  );
-}
