8d74207fd0d0cd953ce46d4b6dca204975898022
Melle <melle@git.com>
2024-10-13 00:28:09 +0200

    Keep homepage in sitemap

--- apps/website/app/sitemap.ts
+++ apps/website/app/sitemap.ts
@@ -4,8 +4,8 @@ import type { MetadataRoute } from "next";
 export const baseUrl = "https://mellevanderlinde.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = Object.entries(navItems).map(([path]) => path);
-
+  const items = { "/": "Home", ...navItems };
+  const routes = Object.entries(items).map(([path]) => path);
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
