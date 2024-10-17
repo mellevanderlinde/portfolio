3336e77a28f46066999de34ef4a918da85bf7529
Melle <melle@git.com>
2024-10-17 17:15:27 +0200

    Add blog posts to sitemap

--- apps/website/app/sitemap.ts
+++ apps/website/app/sitemap.ts
@@ -1,10 +1,19 @@
 import { navItems } from "./components/nav";
 import type { MetadataRoute } from "next";
+import { getBlogPosts } from "./lib/posts";
 
 export const baseUrl = "https://mellevanderlinde.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const items = { "/": "Home", ...navItems };
+  const blogPosts = getBlogPosts().reduce(
+    (acc, post) => {
+      acc[`/blog/${post.slug}`] = post.metadata.title;
+      return acc;
+    },
+    {} as { [key: string]: string },
+  );
+
+  const items = { "/": "Home", ...navItems, ...blogPosts };
   const routes = Object.entries(items).map(([path]) => path);
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
