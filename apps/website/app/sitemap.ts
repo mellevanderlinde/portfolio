8ebf23afbc813f016820b07af44df9c3f482c528
Melle <melle@git.com>
2024-11-01 19:36:33 +0100

    Lock lastmodified time in snapshot

--- apps/website/app/sitemap.ts
+++ apps/website/app/sitemap.ts
@@ -15,8 +15,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   const items = { "/": "Home", ...navItems, ...blogPosts };
   const routes = Object.entries(items).map(([path]) => path);
+
+  const date = new Date();
+  date.setHours(12);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date(),
+    lastModified: date,
   }));
 }
