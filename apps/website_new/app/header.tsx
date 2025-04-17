23406efe164f0dc93da892aa68caadd2370648dd
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-04-17 15:21:19 +0200

    Header

---
+++ apps/website_new/app/header.tsx
@@ -0,0 +1,25 @@
+'use client'
+import type { ReactNode } from 'react'
+import { TextEffect } from '@/components/ui/text-effect'
+import Link from 'next/link'
+
+export function Header(): ReactNode {
+  return (
+    <header className="mb-8 flex items-center justify-between">
+      <div>
+        <Link href="/" className="font-medium text-black dark:text-white">
+          Melle van der Linde
+        </Link>
+        <TextEffect
+          as="p"
+          preset="fade"
+          per="char"
+          className="text-zinc-600 dark:text-zinc-500"
+          delay={0.5}
+        >
+          Cloud Engineer
+        </TextEffect>
+      </div>
+    </header>
+  )
+}
