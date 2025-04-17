5db7cc43821a02c3b5e1e09ab31414dae3a228cb
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-04-17 23:25:01 +0200

    Add blog mdx

--- apps/website_new/mdx-components.tsx
+++ apps/website_new/mdx-components.tsx
@@ -1,5 +1,18 @@
 import type { MDXComponents } from 'mdx/types'
+import type { ReactNode } from 'react'
+import Link from 'next/link'
+
+function CustomLink(props: { href: string, children: ReactNode }): ReactNode {
+  const href = props.href
+  if (href.startsWith('/')) {
+    return <Link {...props}>{props.children}</Link>
+  }
+  if (href.startsWith('#')) {
+    return <a {...props} />
+  }
+  return <a target="_blank" rel="noopener noreferrer" {...props} />
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return components
+  return { ...components, a: CustomLink }
 }
