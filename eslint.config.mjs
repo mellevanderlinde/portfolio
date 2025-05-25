f0f4ec79e936ff2212f6b8b8b6c22cfc8e556cca
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-05-25 12:19:10 +0200

    Simplify eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,8 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    typescript: true,
-    react: true,
-    rules: { 'no-new': 'off' },
-  },
-  {
-    ignores: ['**/cdk.out', 'apps/infra/**/*.js', 'apps/infra/**/*.d.ts', 'apps/website/out/**'],
-  },
-)
+export default antfu({
+  typescript: true,
+  react: true,
+  rules: { 'no-new': 'off' },
+  ignores: ['**/cdk.out', '**/out', 'apps/infra/src/index.js'],
+})
